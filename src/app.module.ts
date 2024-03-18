ba2025133c3b8d40f10748aa5c2adf3b8ba1097f
HyeonJun <gwakhyeonjun@gmail.com>
2024-03-18 15:30:49 +0900

    chore: nestjs/config 모듈 추가 및 SlackModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SlackModule } from 'nestjs-slack-bolt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'env/.env',
+    }),
+    SlackModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
