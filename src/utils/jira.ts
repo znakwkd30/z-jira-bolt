f799ab8860bf738fda686dac82d20878e01542fa
HyeonJun <gwakhyeonjun@gmail.com>
2024-03-22 09:58:44 +0900

    feat: jira file 업데이트 추가

--- src/utils/jira.ts
+++ src/utils/jira.ts
@@ -1,5 +1,11 @@
+import * as superagent from 'superagent';
 import axios, { AxiosResponse } from 'axios';
-import { IJiraIssueRequest, IJiraIssueResponse, IJiraUser } from '../interface';
+import {
+  IJiraFile,
+  IJiraIssueRequest,
+  IJiraIssueResponse,
+  IJiraUser,
+} from '../interface';
 
 export class Jira {
   private readonly baseUrl: string;
@@ -37,5 +43,25 @@ export class Jira {
     );
 
     return result.data;
+  }
+
+  async fileAttachments(issueIdOrKey: string, files: IJiraFile[]) {
+    const request = superagent(
+      'post',
+      `${this.baseUrl}/rest/api/3/issue/${issueIdOrKey}/attachments`,
+    );
+
+    request.set('authorization', `Basic ${this.token}`);
+    request.set('accept', 'application/json');
+    request.set('content-type', 'multipart/form-data');
+    request.set('X-Atlassian-Token', 'no-check');
+
+    for (const file of files) {
+      request.attach('file', file.data, {
+        filename: `${file.name}`,
+      });
+
+      await request;
+    }
   }
 }
