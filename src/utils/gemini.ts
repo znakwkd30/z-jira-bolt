fa953ac054c8b9b123de8b69a37b4e22bea9927f
HyeonJun <gwakhyeonjun@gmail.com>
2024-04-29 10:41:06 +0900

    feat: 한국어로 변경

--- src/utils/gemini.ts
+++ src/utils/gemini.ts
@@ -17,7 +17,7 @@ export class Gemini {
   async run(context: string) {
     const result: GenerateContentResult = await this.model.generateContent(
       context +
-        `Your task is to extract document summaries and descriptions for Jira issue tickets.\n Ensure that summaries and descriptions contain all relevant context needed to interpret them - in other words don't extract small snippets that are missing important context. Provide output in JSON format as follows: \n {"summary": "한글", "description": "한글"}\n Provide summary and description value in Korean.`,
+        `\nJira 이슈 생성을 위해 위에 내용을 요약해서 summary 와 description 추출해줘.\nsummary 와 description 이 내용을 해석 하는데 필요한 관련 모든 컨텍스트를 포함하는지 확인하고 중요한 컨텍스트가 누락된 작은 스니펫을 추출하지 말고 출력은 다음과 같은 JSON format 으로 출력해줘: \n{"summary": "한글", "description": "한글"}`,
     );
     const response: EnhancedGenerateContentResponse = result.response;
 
